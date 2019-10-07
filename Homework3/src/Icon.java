7f45a39f3ba8c3f52639096de5c282a336840687
JessicaHoang <32215166+JessicaHoang@users.noreply.github.com>
2019-10-07 05:24:15 -0700

    Delete Icon.java

--- Homework3/src/Icon.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Component;
-import java.awt.Graphics;
-
-public interface Icon {
-	int getIconWidth();
-	int getIconHeight();
-	void paintIcon(Component c, Graphics g, int x, int y);
-}
