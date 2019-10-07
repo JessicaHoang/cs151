55094789dbdd09f81c6385f06afa99b6bce29f69
JessicaHoang <32215166+JessicaHoang@users.noreply.github.com>
2019-10-07 16:35:33 -0700

    Update SimpleFormatter.java

--- Homework3/5.1/SimpleFormatter.java
+++ Homework3/5.1/SimpleFormatter.java
@@ -3,19 +3,27 @@
 */
 public class SimpleFormatter implements InvoiceFormatter
 {
-Invoice invoice = new Invoice();
-int c = invoice.getCounter();
+
    public String formatHeader()
    {
       total = 0;
       return "     I N V O I C E\n\n\n";
    }
 
-   public String formatLineItem(LineItem item)
+   public String formatLineItem(Invoice in, LineItem item)
    {
+	   
       total += item.getPrice();
+      if(item.toString().equals("Hammer")) {
+      
       return (String.format(
-            "%s: $%.2f (%d)\n",item.toString(),item.getPrice(), c));
+            "%s: $%.2f (%d)\n",item.toString(),item.getPrice(), in.getHammerCounter()));
+      }
+      else if(item.toString().contains("Bundle")){
+    	  return (String.format(
+    	            "%s: $%.2f (%d)\n",item.toString(),item.getPrice(), in.getBundleCounter()));
+      }
+      return "String not found.";
    }
 
    public String formatFooter()
@@ -24,4 +32,5 @@ int c = invoice.getCounter();
    }
 
    private double total;
+
 }
