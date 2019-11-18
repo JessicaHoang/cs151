0690f243e6f0f18ccb7d6ca013f7fc16f58f3acb
JessicaHoang <32215166+JessicaHoang@users.noreply.github.com>
2019-11-17 16:14:37 -0800

    Update hw4.java

--- hw4/1a/hw4.java
+++ hw4/1a/hw4.java
@@ -2,9 +2,9 @@
 public class hw4 {
 	public static void main(String[] args) {
 		bankacc a = new bankacc();
-		final double amount = 100;
-		final int repetitions = 100;
-		final int threads = 100;
+		final double amount = 75;
+		final int repetitions = 75;
+		final int threads = 75;
 		
 		for(int i = 1; i <= threads; i++) {
 			depositRun d = new depositRun(a, amount, repetitions);
